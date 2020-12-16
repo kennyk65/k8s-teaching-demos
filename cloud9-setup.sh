72b0de4daa8700f3e62e2837f1c87599a11ab5c2
kennyk65 <kenkrueger65@gmail.com>
2020-12-16 15:04:15 -0500

    Update cloud9-setup.sh

--- cloud9-setup.sh
+++ cloud9-setup.sh
@@ -4,22 +4,23 @@ echo Installing eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
+
 # Kubectl
 echo Installing Kubectl
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.9/2020-08-04/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin
 kubectl version --short --client
-# Helm
-echo Installing Helm
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-# Display your current identity in the log 
-aws sts get-caller-identity 
-# Configure kubeconfig
-aws eks update-kubeconfig --name ${Cluster}
+
 # Helm
 echo Installing Helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 chmod go-r /home/ssm-user/.kube/config
+
+# Display your current identity in the log 
+aws sts get-caller-identity 
+
+# Configure kubeconfig
+aws eks update-kubeconfig --name ${Cluster}
