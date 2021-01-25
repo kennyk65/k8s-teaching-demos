7480c72eef4ffa0d76e3a29c1a356c06aa5fdefd
EC2 Default User <ec2-user@ip-10-1-10-168.us-west-2.compute.internal>
2021-01-25 15:15:50 +0000

    new

---
+++ setup-cloud9-minikube/setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Kubectl
+if ! command -v kubectl &> /dev/null
+then
+    echo Installing Kubectl
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl    
+    chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin
+else
+    echo Looks like kubectl is already installed.
+fi
+kubectl version --short --client
+
+# helm
+if ! command -v helm &> /dev/null
+then
+    echo Installing Helm
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+    chmod go-r ~/.kube/config
+else
+    echo Looks like Helm is already installed.
+fi
+
+# minikube
+if ! command -v minikube &> /dev/null
+then
+    echo Installing minikube
+    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+    chmod +x ./minikube
+    sudo mv ./minikube /usr/local/bin/
+else
+    echo Looks like minikube is already installed.
+fi
+minikube version
+
+
+# Start minikube
+sudo -i
+minikube start --vm-driver=none
+minikube status
