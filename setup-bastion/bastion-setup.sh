88201c44f23896f99cb1c540a9f528124ca8a260
kennyk65 <kenkrueger65@gmail.com>
2021-11-08 10:51:22 -0500

    Update bastion-setup.sh

--- setup-bastion/bastion-setup.sh
+++ setup-bastion/bastion-setup.sh
@@ -13,7 +13,8 @@ aws configure set region $AWS_REGION
 if ! command -v kubectl &> /dev/null
 then
     echo Installing Kubectl
-    curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.9/2020-08-04/bin/linux/amd64/kubectl
+    # curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.9/2020-08-04/bin/linux/amd64/kubectl
+    curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
     chmod +x ./kubectl
     sudo mv ./kubectl /usr/local/bin
 else
