010ad87c3696614fcead88c4042ac3dd6c3d3d95
KennyK <kenkrueger65@gmail.com>
2021-12-03 12:21:51 -0500

    updates

---
+++ karpenter-demo/setup2.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+# BASED ON:  https://karpenter.sh/docs/getting-started/
+TEMP_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+AWS_ACCOUNT_ID=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|grep accountId|awk -F\" '{print $4}'`
+read -p 'Enter cluster name [primary]: ' CLUSTER_NAME
+CLUSTER_NAME=${CLUSTER_NAME:-primary}
+read -p 'Enter region ['${TEMP_REGION}']: ' AWS_REGION
+read -p 'Enter a comma-separated list of subnet IDs: ' SUBNET_IDS
+read -p 'Enter the name of the IAM role used by the cluster nodes ['${CLUSTER_NAME}-EKSNodeRole']: ' NODE_ROLE
+AWS_REGION=${AWS_REGION:-${TEMP_REGION}}
+NODE_ROLE=${NODE_ROLE:-${CLUSTER_NAME}'-EKSNodeRole'}
+
+echo Using values:  
+echo Account: $AWS_ACCOUNT_ID, Region: $AWS_REGION, Cluster: $CLUSTER_NAME
+echo Subnets: $SUBNET_IDS
+echo Node Role: $NODE_ROLE
+
+# Set region
+echo Setting region to $AWS_REGION
+aws configure set region $AWS_REGION
+
+# Install Kubectl, if needed.
+if ! command -v kubectl &> /dev/null
+then
+    echo Installing Kubectl
+    curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl    
+    chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin
+else
+    echo Looks like kubectl is already installed.
+fi
+kubectl version --short --client
+
+# Install eksctl, if needed.
+if ! command -v eksctl &> /dev/null
+then
+    echo Installing eksctl
+    curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+    sudo mv /tmp/eksctl /usr/local/bin
+else
+    echo Looks like eksctl is already installed.
+fi
+eksctl version
+
+# Install Helm, if needed.
+if ! command -v helm &> /dev/null
+then
+    echo Installing Helm
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+    chmod go-r ~/.kube/config
+else
+    echo Looks like Helm is already installed.
+fi
+
+
+# # Create cluster using eksctl.  Expect this to wait even after CloudFormation is finished.
+# # The withOIDC setting is interesting.  Karpenter will run in a pod and needs to make AWS API calls.
+# cat <<EOF > cluster.yaml
+# ---
+# apiVersion: eksctl.io/v1alpha5
+# kind: ClusterConfig
+# metadata:
+#   name: ${CLUSTER_NAME}
+#   region: ${AWS_DEFAULT_REGION}
+#   version: "1.21"
+# managedNodeGroups:
+#   - instanceType: t3.small
+#     amiFamily: AmazonLinux2
+#     name: ${CLUSTER_NAME}-ng
+#     desiredCapacity: 1
+#     minSize: 1
+#     maxSize: 10
+# iam:
+#   withOIDC: true
+# EOF
+# eksctl create cluster -f cluster.yaml
+
+# Get the subnets created by eksctl and tag them so karpenter can find them:
+echo Tagging subnets to be used by karpenter: $ SUBNET_IDS
+# SUBNET_IDS=$(aws cloudformation describe-stacks \
+#     --stack-name eksctl-ec2-user-karpenter-demo-cluster \
+#     --query 'Stacks[].Outputs[?OutputKey==`SubnetsPrivate`].OutputValue' \
+#     --output text)
+aws ec2 create-tags \
+    --resources $(echo $SUBNET_IDS | tr ',' '\n') \
+    --tags Key="kubernetes.io/cluster/${CLUSTER_NAME}",Value=
+
+# Use CloudFormation to quickly create some extra resources:
+# Create a managed policy to be used by Karpenter
+# Create an instance profile to be used by Karpenter-provisioned EC2 instances:
+cat <<EOF > extra-resources-template.yaml
+AWSTemplateFormatVersion: 2010-09-09
+Resources:
+  KarpenterControllerPolicy:
+    Type: AWS::IAM::ManagedPolicy
+    Properties:
+      ManagedPolicyName: ${ClusterName}-KarpenterControllerPolicy
+      PolicyDocument:
+        Version: 2012-10-17
+        Statement:
+          - Effect: Allow
+            Resource: "*"
+            Action:
+              # Write Operations
+              - ec2:CreateLaunchTemplate
+              - ec2:CreateFleet
+              - ec2:RunInstances
+              - ec2:CreateTags
+              - iam:PassRole
+              - ec2:TerminateInstances
+              # Read Operations
+              - ec2:DescribeLaunchTemplates
+              - ec2:DescribeInstances
+              - ec2:DescribeSecurityGroups
+              - ec2:DescribeSubnets
+              - ec2:DescribeInstanceTypes
+              - ec2:DescribeInstanceTypeOfferings
+              - ec2:DescribeAvailabilityZones
+              - ssm:GetParameter
+  KarpenterInstanceProfile:
+    Type: AWS::IAM::InstanceProfile
+    Properties:
+      InstanceProfileName: ${ClusterName}-KarpenterInstanceProfile
+      Path: /
+      Roles: [ ${NODE_ROLE} ]
+EOF
+aws cloudformation deploy --stack-name Karpenter-${CLUSTER_NAME} --template-file extra-resources-template.yaml   --capabilities CAPABILITY_NAMED_IAM 
+
+# # This maps the new role to an RBAC group.  The new nodes will use this role, existing nodes won't (weird)
+# # TODO: I SHOULD BE ABLE TO REPLACE THIS WITH THE EXISTING ROLE USED BY THE NODEGROUP.
+# # THE EXISTING ROLE USED BY THE NODEGROUP IS ALREADY MAPPED
+# eksctl create iamidentitymapping \
+#   --username system:node:{{EC2PrivateDNSName}} \
+#   --cluster  ${CLUSTER_NAME} \
+#   --arn arn:aws:iam::${AWS_ACCOUNT_ID}:role/KarpenterNodeRole-${CLUSTER_NAME} \
+#   --group system:bootstrappers \
+#   --group system:nodes
+
+# This service account will be used by code in the pod to make AWS API calls to facilitate scaling.
+eksctl create iamserviceaccount --cluster $CLUSTER_NAME --namespace karpenter --name karpenter  \
+  --attach-policy-arn arn:aws:iam::$AWS_ACCOUNT_ID:policy/${ClusterName}-KarpenterControllerPolicy \
+  --approve
+
+# Install Karpenter itself using Helm:
+helm repo add karpenter https://charts.karpenter.sh
+helm repo update
+helm upgrade --install karpenter karpenter/karpenter --namespace karpenter \
+  --create-namespace --set serviceAccount.create=false --version 0.5.0 \
+  --set controller.clusterName=${CLUSTER_NAME} \
+  --set controller.clusterEndpoint=$(aws eks describe-cluster --name ${CLUSTER_NAME} --query "cluster.endpoint" --output json) \
+  --wait # for the defaulting webhook to install before creating a Provisioner
+
+# Create a 'provisioner'.  Note this one is using spot instances:
+cat <<EOF | kubectl apply -f -
+apiVersion: karpenter.sh/v1alpha5
+kind: Provisioner
+metadata:
+  name: default
+spec:
+  requirements:
+    - key: karpenter.sh/capacity-type
+      operator: In
+      values: ["spot"]
+  limits:
+    resources:
+      cpu: 1000
+  provider:
+    instanceProfile: ${ClusterName}-KarpenterInstanceProfile
+  ttlSecondsAfterEmpty: 30
+EOF
+
+echo try running the demo-scale-out.sh and demo-scale-in.sh scripts while watching the number of nodes.
+sleep 5
+
+
