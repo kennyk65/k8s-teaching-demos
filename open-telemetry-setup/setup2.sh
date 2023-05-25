90c80f37734ff8686b83c506c6f90c04d2a49785
Ken Krueger <kenkrueger65@gmail.com>
2023-05-25 12:36:05 -0400

    latest

--- open-telemetry-setup/setup2.sh
+++ open-telemetry-setup/setup2.sh
@@ -7,45 +7,46 @@ read -p 'Enter cluster name [primary]: ' CLUSTER_NAME
 CLUSTER_NAME=${CLUSTER_NAME:-primary}
 AWS_REGION=${AWS_REGION:-${TEMP_REGION}}
 
-# Created AMP workspace
+echo Creat the Amazon Managed Prometheus workspace
 WORKSPACE_ID=$(aws amp create-workspace --alias adot-eks --tags env=workshop --query workspaceId --output text)
-
 sleep 8 # Wait up to 10 seconds for the workspace to become active
 
-#  Setup the OIDC provider on EKS as an identity provider on IAM:
-eksctl utils associate-iam-oidc-provider --cluster $EKS_CLUSTER_NAME --approve     
+echo Setup the OIDC provider on EKS as an identity provider on IAM:
+eksctl utils associate-iam-oidc-provider --cluster $CLUSTER_NAME --approve     
 
-#  Establish a service account matched to IAM Role for pods in the prometheus namespace.
-eksctl create iamserviceaccount --name amp-irsa-role --namespace prometheus --cluster $EKS_CLUSTER_NAME --attach-policy-arn arn:aws:iam::aws:policy/AmazonPrometheusRemoteWriteAccess --approve --override-existing-serviceaccounts
+echo Establish a service account matched to IAM Role for pods in the prometheus namespace.  May not be required if already done.
+eksctl create iamserviceaccount --name amp-irsa-role --namespace prometheus --cluster $CLUSTER_NAME --attach-policy-arn arn:aws:iam::aws:policy/AmazonPrometheusRemoteWriteAccess --approve --override-existing-serviceaccounts
 
-# install cert-manager as its a pre requisite before installing ADOT -
+echo Install cert-manager as its a pre requisite before installing ADOT -
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.2/cert-manager.yaml 
 kubectl get pods -n cert-manager
 
-# Then I install the ADOT addon in my eks cluster.  First permissions then the addon itself:
+echo Install the AWS Distrobution for Open Telemetry addon in the EKS cluster.  First permissions, then the addon itself:
 kubectl apply -f https://amazon-eks.s3.amazonaws.com/docs/addons-otel-permissions.yaml 
-aws eks create-addon --addon-name adot --addon-version v0.74.0-eksbuild.1 --cluster-name $EKS_CLUSTER_NAME
+aws eks create-addon --addon-name adot --addon-version v0.74.0-eksbuild.1 --cluster-name $CLUSTER_NAME
 sleep 5 # Wait for addon to activate:
-aws eks describe-addon --addon-name adot --cluster-name $EKS_CLUSTER_NAME | jq .addon.status
+aws eks describe-addon --addon-name adot --cluster-name $CLUSTER_NAME --output text --query "addon.status"
 sleep 5 # previous status should be "ACTIVE"
 
-# Acquire the Endpoint URL for the Prometheus workspace
-AMP_ENDPOINT_URL=$(aws amp describe-workspace --workspace-id $WORKSPACE_ID | jq '.workspace.prometheusEndpoint' -r)
+echo Acquire the Endpoint URL for the Prometheus workspace
+AMP_ENDPOINT_URL=$(aws amp describe-workspace --workspace-id $WORKSPACE_ID --output text --query 'workspace.prometheusEndpoint')
 AMP_REMOTE_WRITE_URL=${AMP_ENDPOINT_URL}api/v1/remote_write
+echo Endpoint for the Prometheus workspace is $AMP_REMOTE_WRITE_URL
 
-#  Install the Open Telemetry Collector Custom Resource (CR)
+echo Install the Open Telemetry Collector Custom Resource.  Customize the region and URL for our cluster and Prometheus workspace
 curl -O https://raw.githubusercontent.com/aws-containers/eks-app-mesh-polyglot-demo/master/workshop/otel-collector-config.yaml  
-
-# set region code in the yaml file that is downloaded above -
 sed -i -e s/AWS_REGION/$AWS_REGION/g otel-collector-config.yaml
-
-# set AMP remote write URL in the config ymal downloaded above - 
 sed -i -e s^AMP_WORKSPACE_URL^$AMP_REMOTE_WRITE_URL^g otel-collector-config.yaml
-
-# Apply, verify:
 kubectl apply -f ./otel-collector-config.yaml
+sleep 4
 kubectl get all -n prometheus
-sleep 8 # You should see the observability-collector service, deployment, and pod running
+echo We should see the observability-collector service, deployment, and pod running
 
-# Verified the AMP metrics using awscurl:
+echo Test using awscurl, which needs to be installed, which requires an update to Python:
+curl -O https://bootstrap.pypa.io/pip/3.6/get-pip.py    # Get the OLD 3.6 version of get-pip, required by Cloud9
+sudo python3 get-pip.py                             # Install pip for Python 3.6.  This destroys the pre-installed pip
+/usr/local/bin/pip install awscurl              # Install awscurl
+rm get-pip.py                                # Delete the install script.
+
+echo Verify the AMP metrics using awscurl:
 awscurl --service="aps" --region=$AWS_REGION  "${AMP_ENDPOINT_URL}api/v1/query?query=scrape_samples_scraped"
