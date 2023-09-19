25c9ac8837da9f4e5658dec93c5a2783a626f128
Ken Krueger <kenkrueger65@gmail.com>
2023-09-19 16:51:52 -0400

    l

--- prometheus-setup/setup.sh
+++ prometheus-setup/setup.sh
@@ -5,7 +5,11 @@
 kubectl create namespace prometheus
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm upgrade -i prometheus prometheus-community/prometheus --namespace prometheus --set alertmanager.persistentVolume.storageClass="gp2",server.persistentVolume.storageClass="gp2"
-kubectl get pods -n prometheus
-kubectl --namespace=prometheus port-forward deploy/prometheus-server 8080
 
-# point a web browser to http://localhost:8080
+sleep 8 # wait a bit for these pods to start
+kubectl get pods -n prometheus
+
+# WARNING: THIS IS EXPOSING THE PROMETHEUS SERVER ON THE PUBLIC INTERNET WITH NO AUTHENTICATION
+kubectl expose deployment prometheus-server --port=80 --target-port=9090 --name external --type LoadBalancer -n prometheus
+kubectl get svc -n prometheus 
+
