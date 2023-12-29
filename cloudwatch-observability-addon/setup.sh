3bc0070f3d665b99c1f7d60f9d953433ddc03588
kennyk65 <kenkrueger65@gmail.com>
2023-12-29 10:25:31 -0500

    Update setup.sh

--- cloudwatch-observability-addon/setup.sh
+++ cloudwatch-observability-addon/setup.sh
@@ -15,6 +15,7 @@ aws iam attach-role-policy --role-name $AWS_ROLE --policy-arn arn:aws:iam::aws:p
 aws iam attach-role-policy --role-name $AWS_ROLE --policy-arn arn:aws:iam::aws:policy/AWSXrayWriteOnlyAccess
 
 # Install the addon:
-#aws eks create-addon --addon-name amazon-cloudwatch-observability --cluster-name $CLUSTER_NAME
 # TODO: UNTESTED CODE.  SEE SUPPORT ISSUE 170377686801544
-aws eks create-addon --addon-name amazon-cloudwatch-observability --addon-version v1.2.0-eksbuild.1 --cluster-name $CLUSTER_NAME 
+CLUSTER_VERSION=${aws eks describe-cluster --name $CLUSTER_NAME --query 'cluster.version' --ouput text}
+ADD_ON_VERSION=${aws eks describe-addon-versions --kubernetes-version $CLUSTER_VERSION --addon-name amazon-cloudwatch-observability --query 'addons[].addonVersions[0].addonVersion' --output text}
+aws eks create-addon --addon-name amazon-cloudwatch-observability --addon-version $ADD_ON_VERSION --cluster-name $CLUSTER_NAME 
