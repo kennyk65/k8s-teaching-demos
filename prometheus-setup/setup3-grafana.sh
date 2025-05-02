063c4f73fb51b0992faf2f0da6fd23885afd4999
Ken Krueger <kenkrueger65@gmail.com>
2025-05-02 14:30:49 -0400

    added text

--- prometheus-setup/setup3-grafana.sh
+++ prometheus-setup/setup3-grafana.sh
@@ -1,6 +1,6 @@
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-helm install grafana grafana/grafana --namespace grafana
+helm install grafana grafana/grafana --namespace grafana --create-namespace
 
 kubectl port-forward svc/grafana 3000:80 -n grafana
 GRAFANA_PWD=$(kubectl get secret --namespace grafana grafana -o jsonpath="{.data.admin-password}" | base64 -d)
@@ -8,3 +8,8 @@ GRAFANA_PWD=$(kubectl get secret --namespace grafana grafana -o jsonpath="{.data
 echo "Grafana is available at: http://localhost:3000"
 echo "Username: admin"
 echo "Password: $GRAFANA_PWD"
+echo " "
+echo "Once logged in, add prometheus as a datasource."
+echo "  URL: http://prometheus-server.prometheus.svc.cluster.local
+echo "  Name: Prometheus"
+
